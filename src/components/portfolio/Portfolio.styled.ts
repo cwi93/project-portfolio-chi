711fc2b82b55a2c9780ffbb3757d29d48f5d23c4
FlyLikeAChi6 <chi.wai.93@gmail.com>
2025-06-26 15:17:51 +0200

    Draft/pedantic rosalind (#30)
    * Change color design
    
    * Latest build

--- src/components/portfolio/Portfolio.styled.ts
+++ src/components/portfolio/Portfolio.styled.ts
@@ -6,7 +6,7 @@ const CaseContainer = styled.div`
   padding-left: 12px;
 
   h1 {
-    color: #dfc0eb;
+    color: #fff;
     width: 100%;
     padding-left: 12px;
   }
@@ -15,7 +15,6 @@ const CaseContainer = styled.div`
     width: 100%;
     bottom: 0;
     left: 0;
-    background-color: #dfc0eb;
     flex-direction: column;
     white-space: pre-wrap;
   }
@@ -24,12 +23,12 @@ const CaseContainer = styled.div`
     text-transform: none;
     text-align: center;
     margin: 0;
-    color: #fff;
+    color: #090979;
   }
 
   .case-block-role {
     text-transform: uppercase;
-    color: #fff;
+    color: #090979;
     font-size: 1.3rem;
 
     @media only screen and (max-width: 769px) {
@@ -57,11 +56,12 @@ const CaseContainer = styled.div`
   }
 
   .modal-title {
+    color: #090979;
     padding-left: 0;
   }
 
   .company-link {
-    color: #dfc0eb !important;
+    color: #090979 !important;
     text-decoration: none;
   }
 
@@ -81,7 +81,7 @@ const CaseContainer = styled.div`
     padding: 12px 4px;
     margin-right: 8px;
     border-radius: 25px;
-    background-color: #dfc0eb;
+    background-color: #090979;
     color: #fff;
   }
 
@@ -152,8 +152,6 @@ const CaseContainer = styled.div`
 `;
 
 const CaseAnchor = styled.div`
-  background-color: #fff;
-
   @media only screen and (max-width: 769px) {
     padding: 12px 24px;
   }
@@ -169,14 +167,11 @@ const CaseAnchor = styled.div`
 const CaseBlock = styled.a`
   display: flex;
   flex-direction: column;
-  border: solid 2px #dfc0eb;
+  border: solid 2px #fff;
   text-align: center;
   text-decoration: none;
 
-  background-image: linear-gradient(#dfc0eb 100% 100%);
-  background-size: 200%;
-  background-position: 100% 100%;
-  background-repeat: no-repeat;
+  background: #fff;
   transition: background-size 0.3s, background-position 0.3s 0.3s;
 
   &:hover {
@@ -204,7 +199,7 @@ const CaseBlock = styled.a`
       }
 
       .case-block-description {
-        color: #fff;
+        color: #090979;
         position: relative;
 
         animation: float-bottom-to-top 2s forwards;
--- src/styles/Styles.styled.ts
+++ src/styles/Styles.styled.ts
@@ -4,6 +4,13 @@ const PorfolioChi = () => css`
   font-family: Avenir, Montserrat, Corbel, "URW Gothic", source-sans-pro,
     sans-serif;
   color: #000;
+  background: #240120;
+  background: linear-gradient(
+    150deg,
+    rgba(36, 1, 32, 1) 0%,
+    rgba(12, 15, 89, 0.98) 55%,
+    rgba(255, 0, 136, 1) 100%
+  );
 
   overflow: hidden;
 
